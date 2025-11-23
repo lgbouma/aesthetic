1a28f7ef5d8dd0999bcdcbdf22c640b38bfc4cf4
lgbouma <bouma.luke@gmail.com>
2025-11-23 10:30:37 -0800

    simplify test

--- tests/make_test_plot.py
+++ tests/make_test_plot.py
@@ -1,23 +1,12 @@
 from aesthetic.plot import set_style
 
 import numpy as np, matplotlib.pyplot as plt, matplotlib as mpl
-import matplotlib.colors as mcolors
-from cycler import cycler
-
-def set_colors(style):
-    base = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    if '_wob' in style:
-        inverse = [tuple(1 - v for v in mcolors.to_rgb(c)) for c in base]
-        plt.rcParams['axes.prop_cycle'] = cycler('color', inverse)
-    else:
-        pass
 
 def make_plot(style):
     x = np.linspace(0,10,1000)
     y = (x/100)**3 + 5*np.sin(x)
     _x, _y = np.arange(2, 8, 0.5), np.arange(2, 8, 0.5)
 
-    #set_colors(style)
     fig, ax = plt.subplots(figsize=(3,2.5))
     cs = [None]*3 if '_wob' not in style else ['cyan','lime','yellow']
     ax.plot(x, y, label=f'style: {style}', color=cs[0])
